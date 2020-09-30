5ee0da29058b0761690cc60f03f9874608c684d4
KasTheDeveloper <kasra.shirazi.1234@gmail.com>
2020-09-30 11:31:25 +0330

    Spot Upgraded! It gets (i,j) and show x y etc (it contains cost and position of each node)

--- sketch.js
+++ sketch.js
@@ -4,16 +4,27 @@ var grid = new Array(cols);
 
 var openSet = [];
 var closedSet = [];
+var w, h;
+var start;
+var end;
 
-function Spot(){
+function Spot(i, j){
+    this.x = i;
+    this.y = j;
     this.f = 0;
     this.g = 0;
     this.h = 0; 
+    this.show = function(){
+        rect(this.x * w,this.y * h, w, h)
+    }
 }
 
 function setup(){
     createCanvas(400,400);
     console.log('A*');
+    //Creating 2D Array
+    w = width / cols;
+    h = height / rows;
     for (let i = 0; i < cols; i++) {
         grid[i] = new Array(rows);
     }
@@ -23,9 +34,23 @@ function setup(){
             grid[i][j] = new Spot();
         }
     }
+    start = grid[0][0];
+    end = grid[cols - 1][rows - 1];
+    openSet.push(start);
     console.log(grid);
 }
 
 function draw(){
+    if(openSet.length > 0 ){
+
+    } else {
+
+    }
+
     background(0);
+    for(var i = 0; i< cols; i++){
+        for(var j = 0;j < rows; j++){
+            grid[i][j].show();
+        }
+    }
 }
