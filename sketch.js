dccbe8634521d2dc65ba1e27d9e16e44eafec891
KasTheDeveloper <kasra.shirazi.1234@gmail.com>
2020-09-30 10:43:40 +0330

    New Functions Are Added Setup, Draw, Stop etc. "

--- sketch.js
+++ sketch.js
@@ -2,6 +2,12 @@ var cols = 5;
 var rows = 5;
 var grid = new Array(cols);
 
+function Spot(){
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+}
+
 function setup(){
     createCanvas(400,400);
     console.log('A*');
