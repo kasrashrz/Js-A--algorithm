3d936d35cd2563c5367124aeda491ee091a451ab
KasTheDeveloper <kasra.shirazi.1234@gmail.com>
2020-10-01 16:16:12 +0330

    Colors are setting

--- sketch.js
+++ sketch.js
@@ -14,9 +14,9 @@ function Spot(i, j){
     this.f = 0;
     this.g = 0;
     this.h = 0; 
-    this.show = function(){
-        fill(255);
-        stroke(0);
+    this.show = function(color){
+        fill(color);
+        noStroke(0);
         rect(this.x * w ,this.y * h , w - 1 , h - 1);
     }
 }
@@ -54,5 +54,12 @@ function draw(){
         for(var j = 0;j < rows; j++){
             grid[i][j].show(color(255));
         }
+    }
+    for (var i = 0; i < closedSet.length; i++) {
+        closedSet[i].show(color(255, 0, 0))
+    }
+
+    for (var i = 0; i < openSet.length ; i++) {
+       openSet[i].show(color(0, 255, 0))
     }
 }
