af1fc86d0ff587c7023fb3efe56af67162361487
KasTheDeveloper <kasra.shirazi.1234@gmail.com>
2020-10-01 16:33:13 +0330

    addNeighbor added

--- sketch.js
+++ sketch.js
@@ -1,3 +1,11 @@
+function removeFromArray(arr, elt){
+    for (var i = arr.length - 1; i >= 0; i--) {
+        if(arr[i] == elt){
+            arr.splice(i, 1);
+        }
+    }
+}
+
 var cols = 5;
 var rows = 5;
 var grid = new Array(cols);
@@ -14,10 +22,27 @@ function Spot(i, j){
     this.f = 0;
     this.g = 0;
     this.h = 0; 
+    this.neighbors = 0;
     this.show = function(color){
         fill(color);
         noStroke(0);
         rect(this.x * w ,this.y * h , w - 1 , h - 1);
+    }
+
+    this.addNeighbors = function(grid){
+        if(i<cols - 1){
+        this.neighbors.push(grid[this.i + 1, this.j])
+        }
+        if(i > 0){
+        this.neighbors.push(grid[this.i - 1, this.j])
+        }
+        if(j < rows - 1){
+        this.neighbors.push(grid[this.i, this.j + 1])
+        }
+        if(j>0){
+        this.neighbors.push(grid[this.i, this.j - 1])
+        }
+        
     }
 }
 
@@ -44,8 +69,23 @@ function setup(){
 
 function draw(){
     if(openSet.length > 0 ){
-
-    } else {
+        var LI = 0; // contains Lowest Index to the goal or next Node
+        for (var i = 0; i < openSet.length; i++) {
+            if(openSet[i].f < openSet[LI].f){
+                LI = i;
+            }
+        }
+        var current = openSet[LI];
+        //if LI was the end
+        if(current === end){
+            console.log('done');
+        }
+        
+        //As javaScript doesn't have a function to remove somthing from an array we're gonna make an optimal function to do that stuff
+        removeFromArray(openSet,current);
+        closedSet.push(current);
+    
+        } else {
 
     }
 
